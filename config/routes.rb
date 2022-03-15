2aed98b533886be8fb00cf8cf68a6a1010c6ddfe
LoisKOUNINEF <lkouninef@gmail.com>
2022-03-15 08:39:20 +0100

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
-  get 'join_table_products_carts/create'
-  get 'join_table_products_carts/update'
-  get 'join_table_products_carts/destroy'
-  get 'carts/show'
-  get 'carts/create'
-  get 'carts/update'
-  get 'carts/destroy'
   devise_for :users
 
   resources :products
+
+  resources :join_table_products_carts, only: [:create, :update, :destroy]
+  resources :carts, except: [:index, :new, :edit]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
